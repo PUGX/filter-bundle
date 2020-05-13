3bbb58843f5cff55a530f3c49a0cfe22f4789247
Massimiliano Arione <garakkio@gmail.com>
2020-05-13 16:41:38 +0200

    🎨 allow FA icons with CSS

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,9 +1,9 @@
 (function($) {
     $.fn.pugxFilter = function () {
         this.on('hidden.bs.collapse', function () {
-            $('button.filter svg').removeClass('fa-angle-right').addClass('fa-angle-down');
+            $('button.filter svg, button.filter i').removeClass('fa-angle-right').addClass('fa-angle-down');
         }).on('shown.bs.collapse', function () {
-            $('button.filter svg').removeClass('fa-angle-down').addClass('fa-angle-right');
+            $('button.filter svg, button.filter i').removeClass('fa-angle-down').addClass('fa-angle-right');
         });
     };
 }(jQuery));
