05a04f80ccf69e0784cb541b54c364c877d0cc87
Massimiliano Arione <garakkio@gmail.com>
2024-06-04 08:19:51 +0200

    fix deprecated

--- src/DependencyInjection/FilterExtension.php
+++ src/DependencyInjection/FilterExtension.php
@@ -4,15 +4,12 @@ namespace PUGX\FilterBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class FilterExtension extends Extension implements PrependExtensionInterface
 {
-    /**
-     * @param array<string, mixed> $configs
-     */
     public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
